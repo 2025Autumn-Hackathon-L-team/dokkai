7467373cdb75519162eb631dc72c72b4109c36e9
wakana12345 <34619210+wakana12345@users.noreply.github.com>
2025-10-22 12:44:03 +0900

    テーブル名や変数名を変更

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -1,11 +1,12 @@
-
 DROP DATABASE chatapp;
+
 DROP USER 'testuser';
 
 CREATE USER 'testuser' IDENTIFIED BY 'testuser';
+
 CREATE DATABASE chatapp;
-USE chatapp
-GRANT ALL PRIVILEGES ON chatapp.* TO 'testuser';
+
+USE chatapp GRANT ALL PRIVILEGES ON chatapp.* TO 'testuser';
 
 CREATE TABLE users (
     uid VARCHAR(255) PRIMARY KEY,
@@ -14,14 +15,15 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE channels (
+CREATE TABLE bookrooms (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    uid VARCHAR(255) NOT NULL,
-    bookroom_name NVARCHAR(50) NOT NULL,
-    bookroom_description NVARCHAR(1000),
+    user_id VARCHAR(255) NOT NULL,
+    name NVARCHAR(50) NOT NULL,
+    description NVARCHAR(1000),
     is_public BOOLEAN,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(uid) ON DELETE CASCADE
 );
 
 CREATE TABLE messages (
@@ -34,6 +36,32 @@ CREATE TABLE messages (
     FOREIGN KEY (cid) REFERENCES channels(id) ON DELETE CASCADE
 );
 
-INSERT INTO users(uid, user_name, email, password) VALUES('970af84c-dd40-47ff-af23-282b72b7cca8','テスト','test@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578');
-INSERT INTO channels(id, uid, name, abstract) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','ぼっち部屋','テストさんの孤独な部屋です');
-INSERT INTO messages(id, uid, cid, message) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8', '1', '誰かかまってください、、')
+INSERT INTO
+    users(uid, user_name, email, password)
+VALUES
+    (
+        '970af84c-dd40-47ff-af23-282b72b7cca8',
+        'テスト',
+        'test@gmail.com',
+        '37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578'
+    );
+
+INSERT INTO
+    channels(id, uid, name, abstract)
+VALUES
+    (
+        1,
+        '970af84c-dd40-47ff-af23-282b72b7cca8',
+        'ぼっち部屋',
+        'テストさんの孤独な部屋です'
+    );
+
+INSERT INTO
+    messages(id, uid, cid, message)
+VALUES
+    (
+        1,
+        '970af84c-dd40-47ff-af23-282b72b7cca8',
+        '1',
+        '誰かかまってください、、'
+    )
