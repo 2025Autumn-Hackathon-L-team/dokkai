a5005d6ddbefb324f4bd03c12e7d44e513576352
wakana12345 <34619210+wakana12345@users.noreply.github.com>
2025-10-23 16:10:22 +0900

    dockerやenvファイルを変更してコンテナ起動が無事できるようにした。チャンネルも仮データは表示できた

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,24 +1,23 @@
 from flask import Flask, request, redirect, render_template, url_for
 
 from models import Bookroom
-import os
+
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY", uuid.uuid4().hex)
-app.permanent_session_lifetime = timedelta(days=SESSION_DAYS)
 
 
 # ブックルームの一覧表示
 @app.route("/public_bookrooms", methods=["GET"])
 def public_channels_view():
-    # uid = session.get('uid')
-    # if uid is None:
-    #    return redirect(ufl_for('login_view'))
-    # else:
+    # userデータを格納
+    # ユーザデータが今はないのでスキップ
+
+    # publicなブックルームのみ取得
     bookrooms = Bookroom.get_public_bookrooms()
     return render_template("bookroom.html", bookrooms=bookrooms, is_public=True)
 
 
+# ブックルームの作成
 # @app.route('/public_bookrooms', methods=['POST'])
 # def create_bookroom():
 #    bookroom_name = request.form['bookroomname']
--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -1,6 +1,8 @@
 DROP DATABASE chatapp;
-CREATE DATABASE chatapp;
-USE chatapp
+
+CREATE DATABASE IF NOT EXISTS chatapp;
+
+USE chatapp;
 
 CREATE TABLE bookrooms (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -9,9 +11,36 @@ CREATE TABLE bookrooms (
     description NVARCHAR(1000),
     is_public BOOLEAN,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(uid) ON DELETE CASCADE
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO bookrooms(id, uid, bookroom_name, bookroom_description, is_public) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','ハリーポッターと賢者の石','賢者の石について細かく話そう！', TRUE); 
-INSERT INTO bookrooms(id, uid, bookroom_name, bookroom_description, is_public) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','ハリーポッターと秘密の部屋','秘密の部屋について細かく話そう！', FALSE);
+-- FOREIGN KEY (user_id) REFERENCES users(uid) ON DELETE CASCADE
+INSERT INTO
+    bookrooms(
+        user_id,
+        name,
+        description,
+        is_public
+    )
+VALUES
+    (
+        '970af84c-dd40-47ff-af23-282b72b7cca8',
+        'ハリーポッターと賢者の石',
+        '賢者の石について細かく話そう！',
+        TRUE
+    );
+
+INSERT INTO
+    bookrooms(
+        user_id,
+        name,
+        description,
+        is_public
+    )
+VALUES
+    (
+        '970af84c-dd40-47ff-af23-282b72b7cca8',
+        'ハリーポッターと秘密の部屋',
+        '秘密の部屋について細かく話そう！',
+        FALSE
+    );
