1fd1b65e1234ae20a9933f70127f50a9dacffee7
Kyoko-Sakamoto759 <sakamo.1992@gmail.com>
2025-10-26 15:07:37 +0900

    B1-1,2,3のデータベース接続とサーバーの起動まで実装
    （B1-1 データベースT_usersの作成、B1-2 ユーザークラスの作成、B-3 ルーティング設定、サーバーの起動のみ。各処理の記載は未実施）

---
+++ ChatApp/app.py
@@ -0,0 +1,45 @@
+from flask import Flask,render_template,redirect,url_for,session,request
+
+app = Flask(__name__)
+#app.secret_key = 'your_secret_key'
+
+#ルートページのリダイレクト
+@app.route('/')
+def index():
+    #if uid is None:
+    #    return redirect(url_for('login_view'))
+    #return redirect(url_for('channels_view'))
+    return 'Hello World'
+
+#サインアップページの表示
+@app.route('/signup')
+def signup_view():
+    return 'signupの表示'
+
+#サインアップ処理
+@app.route('/signup', methods=['POST'])
+def signup_process():
+    name = request.form.get('name')
+    email =request.form.get('email')
+    return f'{name}と{email}を表示'
+
+#ログインページの表示
+@app.route('/login')
+def login_view():
+    return render_template('auth/login.html')
+
+#ログイン処理
+@app.route('/login', methods=['POST'])
+def login_process():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    return f'{email}と{password}を表示'
+
+#ログアウト処理
+@app.route('/logout',methods = ['POST'])
+def logout():
+    return redirect(url_for('login_view'))
+
+
+if __name__=='__main__':
+    app.run()
---
+++ ChatApp/models.py
@@ -0,0 +1,36 @@
+from flask import abort
+import pymysql
+from util.DB import DB
+
+
+#初期起動時にコネクションプール作成して接続を確立
+db_pool = DB.init_db_pool()
+
+class User:
+    @classmethod
+    def creat(cls,id,name,email,password):  
+        conn = db_pool.get_conn()
+        try:
+            with conn.corsor() as cur:
+                sql = "INSERT INTO users (id,name,email,password)" VALUES (%s,%s,%s,%s);"
+                cur.execute(sql,(id,name,email,password,))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f'エラーが発生しています:{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def find_by_email(cls,email):
+        conn = db_pool.get_conn()
+        try:
+            with conn.corsor() as cur:
+                sql = "SELECT email FROM users WHERE email=%s"
+                cur.execute(sql(email,))
+            return user
+        except pymysql.Error as e:
+            print(f'エラーが発生しています:{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
