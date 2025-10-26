622e2723497ceb3c4a6539918100ac6babaffa1a
wakana12345 <34619210+wakana12345@users.noreply.github.com>
2025-10-27 00:16:31 +0900

    init.sqlから CREATE DATABASESのIF NOT EXISTを消した

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -2,7 +2,7 @@ DROP DATABASE chatapp;
 DROP USER 'testuser';
 
 CREATE USER 'testuser' IDENTIFIED BY 'testuser';
-CREATE DATABASE IF NOT EXISTS chatapp;
+CREATE DATABASE chatapp;
 USE chatapp;
 GRANT ALL PRIVILEGES ON chatapp.* TO 'testuser';
 
