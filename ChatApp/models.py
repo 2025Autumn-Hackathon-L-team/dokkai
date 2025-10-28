8d3c0386a5958a12d22388ac1afad19bebb34963
Kyoko-Sakamoto759 <sakamo.1992@gmail.com>
2025-10-29 04:37:12 +0900

    Update models.py
    model.pyのcreate関数のINSERT文を修正しました、

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -1,32 +1,27 @@
-from flask import abort
-import pymysql
-from util.DB import DB
-
 db_pool = DB.init_db_pool()
 
 class User:
     @classmethod
-    def create(cls,user_id,name,email,password):  
+    def create(cls,id,name,email,password):  
         conn = db_pool.get_conn()
-
-        
         try:
             with conn.cursor() as cur:
                 sql = "INSERT INTO users (id,name,email,password) VALUES (%s,%s,%s,%s);"
-                cur.execute(sql,(user_id,name,email,password,))
+                cur.execute(sql,(id,name,email,password,))
                 conn.commit()
         except pymysql.Error as e:
             print(f'エラーが発生しています:{e}')
             abort(500)
         finally:
             db_pool.release(conn)
+            
 
     @classmethod
     def find_by_email(cls,email):
         conn = db_pool.get_conn()
         try:
             with conn.cursor() as cur:
-                sql = "SELECT email FROM users WHERE email=%s"
+                sql = "SELECT * FROM users WHERE email=%s"
                 cur.execute(sql,(email,))
                 user = cur.fetchone()
             return user
