c9015203701e1b4901a993ed94369f30d824c4c4
linenworld <linenworld.3603@gmail.com>
2025-11-15 14:38:25 +0900

    モーダルが開かなくなるエラーの解決

--- ChatApp/static/js/Profile/update-profile.js
+++ ChatApp/static/js/Profile/update-profile.js
@@ -6,18 +6,21 @@ const updateButton = document.getElementById("update-profile-button");
 const updateProfileModal = document.getElementById("update-profile-modal");
 const updateProfileButtonClose = document.getElementById("update-profile-close-button");
 const flashContent = document.getElementById('modal-flash-content');
-if (flashContent) {
+
+if (flashContent && updateProfileModal) {
+        updateProfileModal.style.display = "flex";
+    
     window.setTimeout(function(){
     flashContent.remove();
   }, 4000);
-}
+}  
 
-if (updateProfileModal) {
-  // モーダル表示ボタンが押された時にモーダルを表示する
+if (updateProfileModal) {  
+  // 必須の追加: モーダル表示ボタンのイベントリスナー
   updateButton.addEventListener("click", () => {
     updateProfileModal.style.display = "flex";
-  });
-
+  });  
+}
   // モーダル内のXボタンが押された時にモーダルを非表示にする
   updateProfileButtonClose.addEventListener("click", () => {
     updateProfileModal.style.display = "none";
@@ -29,7 +32,7 @@ if (updateProfileModal) {
       updateProfileModal.style.display = "none";
     }
   });
-}
+
 
 // これはprofileにあった方がいいか？
 // update-channel-modalが表示されている時に Ctrl/Command + Enter で送信
